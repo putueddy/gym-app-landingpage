b0784b29bf6e3d3f81100cc9608a1074478c540e
I Putu Eddy Irawan <bli.ipei@gmail.com>
2024-03-17 19:17:53 +0700

    Initialize

---
+++ src/components/Hero.svelte
@@ -0,0 +1,31 @@
+<script>
+  import SectionWrapper from "./SectionWrapper.svelte";
+  import Header from "./Header.svelte";
+  import CTAs from "./CTAs.svelte";
+  import Stars from "./Stars.svelte";
+</script>
+
+<SectionWrapper id="hero">
+  <Header />
+  <div
+    class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14"
+  >
+    <h2
+      class="font-semibold text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-center max-w-[1200px] mx-auto w-full"
+    >
+      <span class="text-indigo-800">Gym</span>
+      Training <span class="text-slate-600 line-through">Is Hard</span><br />
+      Just Got <span class="text-indigo-400">Easier</span>
+    </h2>
+    <p
+      class="text-xl sm:text-2xl md:text-3xl text-center max-w-[1000px] px-4 mx-auto w-full"
+    >
+      Less thinking & more doing. We've the trainer in your pocket full of <span
+        class="italic"
+        >personalized workouts, exercise explainations, analytics</span
+      > and much much more.
+    </p>
+    <CTAs />
+    <Stars />
+  </div>
+</SectionWrapper>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Conversion from "../components/Conversion.svelte";
+  import FAQs from "../components/FAQ.svelte";
+  import Hero from "../components/Hero.svelte";
+  import Product from "../components/Product.svelte";
+  import Review from "../components/Review.svelte";
+</script>
+
+<main class="flex flex-col">
+  <Hero />
+  <Product />
+  <Review />
+  <FAQs />
+  <Conversion />
+</main>
